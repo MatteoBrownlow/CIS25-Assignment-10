5372c07112b58c61d622eb77ec0d3fb79878e8be
MatteoBrownlow <114264680+MatteoBrownlow@users.noreply.github.com>
2024-09-16 10:32:26 -0700

    Update activity10.cpp

--- activity10.cpp
+++ activity10.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 int main(){
-     double salesAmount;
+    double salesAmount;
+    cout << "Enter the sales amount: " << endl;
+    cin >> salesAmount;
+    if(salesAmount < 10000){
 
-     return 0;
+    }
+    if(salesAmount <= 15000){
+
+    }
+    
+    return 0;
 }
