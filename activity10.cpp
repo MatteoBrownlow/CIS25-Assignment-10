8d819bca3a8c7b897b11cab352b675b1c2b4cd0f
MatteoBrownlow <114264680+MatteoBrownlow@users.noreply.github.com>
2024-09-16 10:36:29 -0700

    Update activity10.cpp

--- activity10.cpp
+++ activity10.cpp
@@ -6,11 +6,13 @@ int main(){
     cout << "Enter the sales amount: " << endl;
     cin >> salesAmount;
     if(salesAmount < 10000){
-
+        cout << "Your commission is " << salesAmount / 10;
+        return 0;
     }
     if(salesAmount <= 15000){
-
+        cout << "Your commission is " << salesAmount * 1.5 / 10;
+        return 0;
     }
-    
+    cout << "Your commission is " << salesAmount / 5;
     return 0;
 }
